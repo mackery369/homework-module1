8cb8f024aed36c0d2ae82c641d03807b4af736e4
Nathanaell Kay <nathanaellkay@Nathanaells-MacBook-Air.local>
2022-11-27 14:57:29 -0600

    Switching codes

--- site_scripts.js
+++ site_scripts.js
@@ -22,3 +22,13 @@
 // link.rel= 'stylesheet';
 // link.type = 'img/css';
 // link.href = 'styles.css';
+
+
+/* -- Random Codes -- */
+//Note: submit button is initially disabled upon loading of this page - see <body> in html
+
+var code = ' '; //to store generated codes and initialize to empty value
+var getCode = ' '; //to store entered code
+var btnvalue; //for button boolean value
+//create variable to hold the type of characters we want to show as codes
+var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$"

e33b05d36c83cd1871a51615b6e8c504478e248c
Nathanaell Kay <nathanaellkay@Nathanaells-MacBook-Air.local>
2022-11-27 15:06:51 -0600

    Updated button through body id onLoad

--- randomcodes.js
+++ randomcodes.js
@@ -18,8 +18,19 @@ function generateCode() {
 document.getElementById("codes").innerHTML = generateCode();
 
 //Disable Button
-function disableButton() {
-    document.getElementById("submit").disabled = true;
+//determine when to able or disable button
+function disableButton(btnvalue) {
+    document.getElementById("submit").disabled = btnvalue; //able/disable button
+    if (btnvalue == true) { //test if button is disabled or enabled
+        //set button and label color translucent
+        document.getElementById("submit").style.backgroudColor = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 0.5)";
+    } else {
+        //set button and label color with no transparency
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 1)";
+    }
+
 }
 
 //Activate function
